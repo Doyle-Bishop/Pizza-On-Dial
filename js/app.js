8fb31ef54abebc39eb67cf9a611d70f6c27bc3cb
hamsoace <hamsoace@gmail.com>
2020-09-08 11:59:35 +0300

    popup form styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
+// document.getElementById("cart-info").addEventListener("click", function() {
+//   const cart = document.getElementById("cart");
+//   cart.classList.toggle("show-cart");
+//   console.log(cart);
+// });
+
+$(document).ready(function () {
+  $(".popup-btn").click(function () {
+    $(".popup")
+      .css("display", "flex")
+      .css("animation", "popup-bg 1s")
+      .css("animation-interaction-count", "1");
+    $(".popup-wrap")
+      .css("animation", "popup 1s")
+      .css("animation-iteration-count", "1");
+  });
+
+  $(".close").click(function () {
+    $(".popup-wrap")
+      .css("animation", "popup 1s")
+      .css("animation-iteration-count", "1");
+    $(".popup").delay(500).fadeOut();
+  });
 });
